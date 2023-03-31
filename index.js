9ad71522923240d8a5146a3c5e9287d76edce880
leslie <alvarado.leslie12@yahoo.com>
2023-03-31 12:35:09 -0700

    lab

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
-// Write your code here
+let num1 = 31;
+let num2 = 2;
+
+let multiply = num1 * num2
+
+//let random;
+
+//Math.random() + 1;
+
+let num3 = 22;
+let num4 = 6;
+
+let mod = num3 % num4;
+
+let max = 20;
+Math.max(1,19,20);
+
+
+
+let random = Math.floor(Math.random() * 5) + 1;
+Math.floor(Math.random() * 5) + 1;
